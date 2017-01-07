d95e4ed6695406fb79784539db2a82025063faf4
Andrei Bencsik <andrei.bencsik@gmail.com>
2017-01-07 15:53:32 +0100

    Merging the expressions and statements; for the moment, postfix works, unary not works

---
+++ Parsing/expressions_tests/statementExpressionTest.java
@@ -0,0 +1,38 @@
+ {;}
+ { a:}
+ { case a: }
+ { default : }
+{ break a; }
+{ continue a; }
+{ break; }
+{ continue; }
+{ return a; }
+{ return; }
+{ 
+	try {
+		;
+	} finally { 
+		;
+	}
+}
+{
+	if(a.b) {
+		try {
+		;
+		} finally { 
+			;
+		}
+	} 
+	else { ; }
+	int b=a;
+	for(int a=c.d;a; a.b) {
+	}
+}
+{
+	int a;
+	double d;
+	for(int a;!a;a++) {
+	;
+	}
+	if(!a) { ; }
+}
