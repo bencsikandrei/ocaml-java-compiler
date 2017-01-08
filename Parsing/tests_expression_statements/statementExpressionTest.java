8080858303da187ff55c89ca7c9e21db704e2e1f
Andrei Bencsik <andrei.bencsik@gmail.com>
2017-01-08 17:22:47 +0100

    Added tests for statement expressions; found a bug

--- Parsing/tests_expression_statements/statementExpressionTest.java
+++ Parsing/tests_expression_statements/statementExpressionTest.java
@@ -1,33 +1,3 @@
- {;}
- { a:}
- { case a: }
- { default : }
-{ break a; }
-{ continue a; }
-{ break; }
-{ continue; }
-{ return a; }
-{ return; }
-{ 
-	try {
-		;
-	} finally { 
-		;
-	}
-}
-{
-	if(a.b) {
-		try {
-		;
-		} finally { 
-			;
-		}
-	} 
-	else { ; }
-	int b=a;
-	for(int a=c.d;a; a.b) {
-	}
-}
 {
 	char bc = a << 10;
 	char bc = a >> 10;
