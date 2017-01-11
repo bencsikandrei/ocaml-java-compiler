cbdc2487cff7587c1a79c9e3fb898b07997d3c7d
Andrei Bencsik <andrei.bencsik@gmail.com>
2017-01-11 23:35:11 +0100

    Added a for test

---
+++ Parsing/tests_expression_statements/statementFor.java
@@ -0,0 +1,23 @@
+{
+	for(;;) {
+	}
+	
+	for(;;);
+	
+	for(int i =1; i < 10; i++) {
+		;
+	}
+	
+	for(int i : array ) {
+		i++;
+	}
+	
+	for(;true;) {
+		;
+	}	
+	
+	for(int i = 10, j=20;i<j;j--,i++) {
+		
+	}
+}
+
