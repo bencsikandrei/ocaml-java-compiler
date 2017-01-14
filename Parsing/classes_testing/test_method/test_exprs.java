fd62259a6059a2bc70c878786b70e86aebfcb192
Florencia Alvarez Etcheverry <florencia.alvarezetcheverry@telecom-bretagne.eu>
2017-01-14 21:56:23 +0100

    test fixes and tree printing for file, interface

--- Parsing/classes_testing/test_method/test_exprs.java
+++ Parsing/classes_testing/test_method/test_exprs.java
@@ -1,10 +1,2 @@
-public void main (){}
-public void main (){2+2;3-3;}
-public double main (){a/22;2+2;}
-public char main (char name){}
-public string main (double name,name2 name){a/22;2+2;}
-public static void main (double name,name2 name,char name3);
-public public public static public  void main (double name,name2 name,char name3);
-public static abstract <type> void main (double name,name2 name,char name3);
-public static abstract <type1,type2,typ3> void main (double name,name2 name,char name3);
-public static abstract <type1,type2,typ3> type1 main (double name,name2 name,char name3) throws beep;
+public string main (double name,name2 name){int a = 3;;};
+
