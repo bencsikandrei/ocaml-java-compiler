9a24bd4eac30bbfbaf62ed724239ffdc12e01112
Florencia Alvarez Etcheverry <florencia.alvarezetcheverry@telecom-bretagne.eu>
2017-01-15 12:39:54 +0100

    added optional semicolons at the end of classes, and print java file

--- Parsing/printing.ml
+++ Parsing/printing.ml
@@ -275,7 +275,7 @@ and print_java_class var =
 	"\nType Parameters: "^(print_list print_type_param var.ctparam " ")^
 	"\nParent: "^(print_parent var.cparent)^
 	"\nInterfaces: "^(String.concat ", " var.cinterfaces)^
-	"\nBody: "^(print_list print_inside_class var.cbody " ")
+	"\nBody: "^(print_list print_inside_class var.cbody " ")^" -----------------\n"
 
 and print_fcontent var = match var with
 	| F_Class(c) -> print_java_class c
@@ -302,4 +302,4 @@ let print_import var =
 let prit_java_file var =
 	"\nPackage: "^(String.concat ", " var.fPackage)^
 	"\nImports: "^(print_list print_import var.fImports " ")^
-	"\nFile Content: "^(print_list print_fcontent var.fContent " ")
+	"\nFile Content: "^(print_list print_fcontent var.fContent " ")^"\n\n"
